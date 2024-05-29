2f0f0dfb4c553a97719aa187065e7553a9201683
PuwaritK <puwarit420@gmail.com>
2024-05-30 03:29:01 +0700

    fix: fix board not updating when moving piece

--- chessgame/board.py
+++ chessgame/board.py
@@ -1,5 +1,5 @@
 import pygame
-from .piece import Piece
+from .piece import Piece, PieceColor
 
 TILES_COUNT = 8
 
@@ -15,10 +15,24 @@ class Board:
             self.tiles.append([None] * TILES_COUNT)
 
     def move_piece(self, x1: int, y1: int, x2: int, y2: int):
-        self.tiles[x2][y2] = self.get_piece(x1, y1)
+        moved_piece = self.get_piece(x1, y1)
+        if moved_piece is None:
+            raise ValueError("you tried to move nothing")
+        moved_piece.posX = x2
+        moved_piece.posY = y2
+        target = self.tiles[x2][y2]
+        if target is None:
+            self.tiles[x1][y1] = None
+            return
+        if moved_piece.color == target.color:
+            raise Exception("you killed your own kind")
+        if target.color == PieceColor.WHITE:
+            self.white_count -= 1
+        elif target.color == PieceColor.BLACK:
+            self.black_count -= 1
         self.tiles[x1][y1] = None
 
     def get_piece(self, x: int, y: int) -> Piece | None:
         if x < 0 or y < 0:
-            raise KeyError("you moved the piece out of the board")
+            raise KeyError("you tried to get invalid piece")
         return self.tiles[x][y]
