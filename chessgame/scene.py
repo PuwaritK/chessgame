f86d7423d73c1311b35b0aecd7af160f603ba0d4
PuwaritK <puwarit420@gmail.com>
2024-12-22 23:23:16 +0700

    feat: add 'change time control' setting in setting scene

--- chessgame/scene.py
+++ chessgame/scene.py
@@ -20,7 +20,7 @@ GAME_FONT = "Sans Serif"
 BUTTON_TEXT_SIZE = 50
 WINNER_SCENE_TEXT_SIZE = 150
 MENU_FONT = 70
-TIME_CONTROL = (5, 0)
+TIME_CONTROL = [15, 10]  # 15 minutes | 10 seconds increment
 
 
 class Scene(ABC):
@@ -77,8 +77,10 @@ class GameScene(Scene):
             self.available_moves = None
             self.piece = None
             if self.turn == PieceColor.WHITE:
+                self.white_time -= TIME_CONTROL[1]
                 self.turn = PieceColor.BLACK
             elif self.turn == PieceColor.BLACK:
+                self.black_time -= TIME_CONTROL[1]
                 self.turn = PieceColor.WHITE
             if not can_continue(self.board, self.turn):
                 winner = get_winner(self.board, self.turn)
@@ -93,6 +95,10 @@ class GameScene(Scene):
 
     def on_loop(self, screen: pygame.Surface, delta_time: float):
         screen.fill(BACKGROUND_COLOR)
+        if self.white_time > TIME_CONTROL[0] * 60:
+            return GameOverScene(PieceColor.BLACK)
+        if self.black_time > TIME_CONTROL[0] * 60:
+            return GameOverScene(PieceColor.WHITE)
         self.pos_and_size = background.draw_checkers(screen, self.available_moves)
         render_pieces(screen, self.board.tiles, self.pos_and_size, self.images)
         if self.board.promoted_piece is not None:
@@ -104,6 +110,10 @@ class GameScene(Scene):
             self.promotion_offset_x, self.promotion_offset_y = render_promotion(
                 screen, self.board.promoted_piece, self.pos_and_size, self.images
             )
+        if self.turn == PieceColor.WHITE:
+            self.white_time += delta_time
+        elif self.turn == PieceColor.BLACK:
+            self.black_time += delta_time
 
 
 class MenuScene(Scene):
@@ -143,12 +153,94 @@ class MenuScene(Scene):
 
 
 class SettingScene(Scene):
+    def __init__(self) -> None:
+        self.menu_font = pygame.font.SysFont(GAME_FONT, MENU_FONT)
+        self.button_font = pygame.font.SysFont(GAME_FONT, BUTTON_TEXT_SIZE)
+        self.configuring_time_control = False
 
     def on_click(self, delta_time: float) -> "Scene | None":
-        pass
+        pos_x, pos_y = pygame.mouse.get_pos()
+
+        if self.time_control_button_rect.collidepoint(pos_x, pos_y):
+            self.configuring_time_control = True
+            return
+        if self.return_button_rect.collidepoint(pos_x, pos_y):
+            return MenuScene()
+
+        if self.configuring_time_control:
+            if self.confirm_button_rect.collidepoint(pos_x, pos_y):
+                self.configuring_time_control = False
+
+            if self.minute_up_button_rect.collidepoint(pos_x, pos_y):
+                TIME_CONTROL[0] += 1
+            if self.minute_down_button_rect.collidepoint(pos_x, pos_y):
+                TIME_CONTROL[0] -= 1
+
+            if self.second_up_button_rect.collidepoint(pos_x, pos_y):
+                TIME_CONTROL[1] += 1
+            if self.second_down_button_rect.collidepoint(pos_x, pos_y):
+                TIME_CONTROL[1] -= 1
 
     def on_loop(self, screen: pygame.Surface, delta_time: float):
-        pass
+        screen.fill(BACKGROUND_COLOR)
+        if not self.configuring_time_control:
+            time_control_button = self.button_font.render(
+                "Configure Time Control", True, (0, 0, 0)
+            )
+            self.time_control_button_rect = time_control_button.get_rect(
+                center=(screen.get_width() / 2, screen.get_height() / 2)
+            )
+            return_button = self.button_font.render("Return", True, (0, 0, 0))
+            self.return_button_rect = return_button.get_rect(
+                center=(screen.get_width() / 2, screen.get_height() * 3 / 4)
+            )
+            screen.blit(time_control_button, self.time_control_button_rect)
+            screen.blit(return_button, self.return_button_rect)
+        else:
+            time_control_text = self.menu_font.render(
+                f"{TIME_CONTROL[0]} | {TIME_CONTROL[1]}", True, (0, 0, 0)
+            )
+            time_control_text_rect = time_control_text.get_rect(
+                center=(screen.get_width() / 2, screen.get_height() / 2)
+            )
+            screen.blit(time_control_text, time_control_text_rect)
+
+            minute_up_button = self.button_font.render(
+                "Increase Minutes", True, (0, 0, 0)
+            )
+            self.minute_up_button_rect = minute_up_button.get_rect(
+                center=(screen.get_width() * 1 / 8, screen.get_height() * 3 / 4)
+            )
+            minute_down_button = self.button_font.render(
+                "Decrease Minutes", True, (0, 0, 0)
+            )
+            self.minute_down_button_rect = minute_down_button.get_rect(
+                center=(screen.get_width() * 3 / 8, screen.get_height() * 3 / 4)
+            )
+
+            second_up_button = self.button_font.render(
+                "Increase Seconds", True, (0, 0, 0)
+            )
+            self.second_up_button_rect = second_up_button.get_rect(
+                center=(screen.get_width() * 5 / 8, screen.get_height() * 3 / 4)
+            )
+            second_down_button = self.button_font.render(
+                "Decrease Seconds", True, (0, 0, 0)
+            )
+            self.second_down_button_rect = second_down_button.get_rect(
+                center=(screen.get_width() * 7 / 8, screen.get_height() * 3 / 4)
+            )
+
+            confirm_button = self.button_font.render("Confirm Change", True, (0, 0, 0))
+            self.confirm_button_rect = confirm_button.get_rect(
+                center=(screen.get_width() * 1 / 2, screen.get_height() * 7 / 8)
+            )
+
+            screen.blit(minute_up_button, self.minute_up_button_rect)
+            screen.blit(minute_down_button, self.minute_down_button_rect)
+            screen.blit(second_up_button, self.second_up_button_rect)
+            screen.blit(second_down_button, self.second_down_button_rect)
+            screen.blit(confirm_button, self.confirm_button_rect)
 
 
 class GameOverScene(Scene):
